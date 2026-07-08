89de7890ef048723c65575de77a131f6332a6cdb
yu <yurika0uu@gmail.com>
2026-07-08 08:40:35 +0700

    Delete customize.sh

--- customize.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-ui_print "Seiran Sysctl Guard v0.0.2"
-ui_print "device : $(getprop ro.product.device)"
-ui_print "kernel : $(uname -r)"
-set_perm_recursive "$MODPATH" root root 0755 0644
-set_perm "$MODPATH/service.sh" root root 0755
-set_perm "$MODPATH/apply.sh"   root root 0755
-set_perm "$MODPATH/action.sh"  root root 0755
-ui_print "reboot to apply. open WebUI in KSU Manager to configure TCP."
